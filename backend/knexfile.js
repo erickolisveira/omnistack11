b32fbfa7171edc76622ca89ce9af068dc3030ffd
Erick Oliveira <erickoliveiraah@gmail.com>
2020-03-26 13:51:26 -0300

    Backend finalizado

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './src/databse/db.sqlite'
+      filename: './src/database/db.sqlite'
     },
     migrations: {
       directory: './src/database/migrations'
